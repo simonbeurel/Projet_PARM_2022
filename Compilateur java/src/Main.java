1c25c76527957259630d9ad3882b092811042623
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2023-01-13 10:47:48 +0100

    add branch specifications

--- Compilateur java/src/Main.java
+++ Compilateur java/src/Main.java
@@ -1,13 +1,17 @@
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.*;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
-        //Compilator c = new Compilator();
-        //c.process("C:/Users/Proprietaire/Desktop/Compilateur java/src/test.txt");
+        /**Compilator c = new Compilator();
+        c.process("C:/Users/Proprietaire/Desktop/Compilateur java/src/test.txt");*/
 
-        String s = "0001100001110111";
-        int val = Integer.parseInt(s,2);
-        System.out.println(Integer.toHexString(val));
+        FileInputStream file = new FileInputStream("C:/Users/Proprietaire/Desktop/Projet_PARM_2022/Compilateur java/src/test.txt");
+        HashMap<String,Integer> branchTable = new HashMap<String,Integer>();
+        branchTable = Engine.scanBranch(file);
+        System.out.println(branchTable);
+
+
     }
 }
